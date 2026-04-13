dc143185524ea8fd5ca5d7320df24353926d386b
irjakniibos <irza.yaumil.ft23@mail.umy.ac.id>
2026-04-13 10:01:05 +0700

    Add login endpoint and basic authentication

---
+++ src/main/java/com/deploy/praktikum6/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.deploy.praktikum6.controller;
+
+
+import com.deploy.praktikum6.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    List<User> dataUser = new ArrayList<>();
+
+    @GetMapping("/")
+    public String login(){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String prosesLogin(
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model){
+
+        if(username.equals("admin") && password.equals("123456")){
+            model.addAttribute("data", dataUser);
+            return "home";
+        }
+
+        model.addAttribute("error","Username atau Password salah");
+        return "login";
+    }
+
+}
